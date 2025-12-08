381ae44e07cc0235ef60f3d8c9f394e1abaa2789
Jan Lemsky <jan.lemsky@mgm-tp.com>
2025-12-08 16:13:45 +0100

    Added christmas wishes as external enumerations from A12 documents

--- client/src/components/PersonFormEngineView.tsx
+++ client/src/components/PersonFormEngineView.tsx
@@ -4,30 +4,52 @@ import { ReadonlyObjectMap } from "@com.mgmtp.a12.formengine/formengine-core/lib
 import { View } from "@com.mgmtp.a12.client/client-core/lib/core/view";
 import IExternalEnumerationProvider from "@com.mgmtp.a12.formengine/formengine-core/lib/back-end/services/external-enumeration-provider";
 import { FormEngineViews } from "@com.mgmtp.a12.formengine/formengine-core/lib/client-extensions";
-import { ConnectorLocator, RestServerConnector } from "@com.mgmtp.a12.utils/utils-connector/lib/main";
+import {
+    Dispatcher,
+    QueryJsonRpc2Request,
+    QueryJsonRpc2Response
+} from "@com.mgmtp.a12.dataservices/dataservices-access";
 
 type ExternalEnumerationMap = ReadonlyObjectMap<{ [key: string]: string | undefined }>;
 
-interface Wish {
-    de: string;
-    en: string;
+interface WishDocument {
+    Christmas: {
+        Wish: {
+            de: string;
+            en: string;
+        };
+    };
 }
 
-async function loadWishes(): Promise<Wish[]> {
-    const connector = ConnectorLocator.getInstance().getServerConnector() as RestServerConnector;
-    return connector
-        .fetchData({
-            relativeUrl: "/christmas-wishes.json",
-            method: "GET"
-        })
-        .then((response) => response.json());
+async function loadWishes(): Promise<WishDocument[]> {
+    const request: QueryJsonRpc2Request = {
+        method: "QUERY",
+        jsonrpc: "2.0",
+        id: "loadWishes",
+        params: {
+            query: {
+                targetDocumentModel: "ChristmasWish_DM",
+                projectionName: "document",
+                paging: {
+                    pageNumber: 0,
+                    pageSize: 100
+                }
+            }
+        }
+    };
+
+    const [response] = await Dispatcher.rpc("en", [request]);
+
+    return response && QueryJsonRpc2Response.isInstance(response)
+        ? ((response.result.entries?.map((entry) => entry?.document) ?? []) as WishDocument[])
+        : [];
 }
 
-function mapWishesToExternalEnum(wishes: Wish[]): ExternalEnumerationMap {
+function mapWishesToExternalEnum(wishes: WishDocument[]): ExternalEnumerationMap {
     return wishes.reduce(
         (result, wish, id) => ({
             ...result,
-            [id]: { en: wish.en, de: wish.de }
+            [id]: wish.Christmas.Wish
         }),
         {}
     );
