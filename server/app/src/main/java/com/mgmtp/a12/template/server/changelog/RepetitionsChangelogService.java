61f09f4dd176fda3efa08193601007ce45602d69
Gildardo Morga-Ramirez <gildardo.morga-ramirez@mgm-tp.com>
2026-02-23 17:33:01 +0100

    start of the changelog implementation for repetitions of repeatable groups

--- server/app/src/main/java/com/mgmtp/a12/template/server/changelog/RepetitionsChangelogService.java
+++ server/app/src/main/java/com/mgmtp/a12/template/server/changelog/RepetitionsChangelogService.java
@@ -1,9 +1,19 @@
 package com.mgmtp.a12.template.server.changelog;
 
+import com.mgmtp.a12.kernel.md.document.apiV2.DocumentMultiPointer;
+import com.mgmtp.a12.kernel.md.document.apiV2.DocumentPointer;
 import com.mgmtp.a12.kernel.md.document.apiV2.immutable.DocumentV2;
+import com.mgmtp.a12.kernel.md.document.apiV2.immutable.GroupInstanceV2;
+import com.mgmtp.a12.kernel.md.document.apiV2.utils.DocumentV2Utils;
+import com.mgmtp.a12.template.server.typings.views._changemetadata_dm.ChangeMetadata;
+import com.mgmtp.a12.template.server.typings.views._repetitionchange_dm.RepetitionChange;
+import com.mgmtp.a12.template.server.typings.views._repetitionchange_dm._repetitionchange.ChangeType;
 
 import java.time.Instant;
+import java.util.Map;
 import java.util.Set;
+
+import static com.mgmtp.a12.template.server.utils.Constants.ID_FIELD_NAME;
 
 class RepetitionsChangelogService extends CommonChangelogService {
 
@@ -21,6 +31,28 @@ class RepetitionsChangelogService extends CommonChangelogService {
         for (String groupPath : groupPathsWithChangelog) {
 
             // extract the group instances from document and reference (be sure to use wildcards - ofPathWithAllWildcards)
+            DocumentMultiPointer pointer = DocumentMultiPointer.ofPathWithAllWildcards(groupPath);
+            var groupInstancesDoc = DocumentV2Utils.getGroupInstances(document, pointer);
+            var groupInstancesRef = DocumentV2Utils.getGroupInstances(reference, pointer);
+
+            for (Map.Entry<DocumentPointer, GroupInstanceV2> groupInstanceEntry : groupInstancesDoc) {
+                String id = (String) groupInstanceEntry.getValue().directFieldValue(ID_FIELD_NAME);
+                var entryRef = groupInstancesRef.stream()
+                        .filter(e -> {
+                            String refId = (String) e.getValue().directFieldValue(ID_FIELD_NAME);
+                            return id.equals(refId);
+                        })
+                        .findFirst();
+                if (entryRef.isEmpty()) {
+                    // added
+                    var changelogEntry = RepetitionChange._empty()
+                            ._with(RepetitionChange._pointer().changeType(), ChangeType.ADDED)
+                            ._with(RepetitionChange._pointer().repetitionDetails(), "");
+                    ChangeMetadata._empty()
+                            ._with(ChangeMetadata._pointer().changedAt(), now)
+                            ._with(ChangeMetadata._pointer().changedBy(), userName);
+                }
+            }
 
             // iterate over the group instances from document
 
