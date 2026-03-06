de477c4628d4a210f7ab4081873a925368d29255
Gildardo Morga-Ramirez <gildardo.morga-ramirez@mgm-tp.com>
2026-03-06 14:37:50 +0100

    finish up the changelog implementation for repetitions of repeatable groups

--- server/app/src/main/java/com/mgmtp/a12/template/server/changelog/RepetitionsChangelogService.java
+++ server/app/src/main/java/com/mgmtp/a12/template/server/changelog/RepetitionsChangelogService.java
@@ -12,8 +12,11 @@ import com.mgmtp.a12.template.server.typings.views._repetitionchange_dm._repetit
 import java.time.Instant;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import static com.mgmtp.a12.template.server.utils.Constants.CHANGELOG_GROUP_SUFFIX;
 import static com.mgmtp.a12.template.server.utils.Constants.ID_FIELD_NAME;
+import static com.mgmtp.a12.template.server.utils.Constants.POINTER_WILDCARD;
 
 class RepetitionsChangelogService extends CommonChangelogService {
 
@@ -30,41 +33,74 @@ class RepetitionsChangelogService extends CommonChangelogService {
 
         for (String groupPath : groupPathsWithChangelog) {
 
-            // extract the group instances from document and reference (be sure to use wildcards - ofPathWithAllWildcards)
             DocumentMultiPointer pointer = DocumentMultiPointer.ofPathWithAllWildcards(groupPath);
+            // use document and not result to look up into the original state
             var groupInstancesDoc = DocumentV2Utils.getGroupInstances(document, pointer);
             var groupInstancesRef = DocumentV2Utils.getGroupInstances(reference, pointer);
 
             for (Map.Entry<DocumentPointer, GroupInstanceV2> groupInstanceEntry : groupInstancesDoc) {
-                String id = (String) groupInstanceEntry.getValue().directFieldValue(ID_FIELD_NAME);
+                DocumentPointer groupPointer = groupInstanceEntry.getKey();
+                GroupInstanceV2 groupInstance = groupInstanceEntry.getValue();
+                // method fieldValue would also work
+                String groupInstanceId = (String) groupInstance.directFieldValue(ID_FIELD_NAME);
                 var entryRef = groupInstancesRef.stream()
                         .filter(e -> {
-                            String refId = (String) e.getValue().directFieldValue(ID_FIELD_NAME);
-                            return id.equals(refId);
+                            String groupInstanceIdRef = (String) e.getValue().directFieldValue(ID_FIELD_NAME);
+                            return groupInstanceId.equals(groupInstanceIdRef);
                         })
                         .findFirst();
                 if (entryRef.isEmpty()) {
-                    // added
-                    var changelogEntry = RepetitionChange._empty()
-                            ._with(RepetitionChange._pointer().changeType(), ChangeType.ADDED)
-                            ._with(RepetitionChange._pointer().repetitionDetails(), "");
-                    ChangeMetadata._empty()
-                            ._with(ChangeMetadata._pointer().changedAt(), now)
-                            ._with(ChangeMetadata._pointer().changedBy(), userName);
+                    // new repetition
+                    result = handleRepetitionChange(result, groupPointer, groupInstance, ChangeType.ADDED);
+                } else {
+                    // changed -> we don't care, let's remove the map entry
+                    groupInstancesRef.remove(entryRef.get());
                 }
             }
-
-            // iterate over the group instances from document
-
-            // get group id and also from reference
-
-            // compare existence (consider deleted ones)
-
-            // build RepetitionChange using TAC
-            // implement repetition details
+            for (Map.Entry<DocumentPointer, GroupInstanceV2> removed : groupInstancesRef) {
+                DocumentPointer groupPointer = removed.getKey();
+                GroupInstanceV2 groupInstance = removed.getValue();
+                result = handleRepetitionChange(result, groupPointer, groupInstance, ChangeType.DELETED);
+            }
         }
 
         return result;
+    }
+
+    private DocumentV2 handleRepetitionChange(
+            DocumentV2 result,
+            DocumentPointer groupPointer,
+            GroupInstanceV2 groupInstance,
+            ChangeType changeType
+    ) {
+        var rcp = RepetitionChange._pointer();
+        String repetitionDetails = createRepetitionDetails(groupInstance);
+        var changelogEntry = RepetitionChange._empty()
+                ._with(rcp.changeType(), changeType)
+                ._with(rcp.repetitionDetails(), repetitionDetails);
+        var pcm = ChangeMetadata._pointer();
+        ChangeMetadata changeMeta = ChangeMetadata._empty()
+                ._with(pcm.changedBy(), userName)
+                ._with(pcm.changedAt(), now);
+
+        changelogEntry = changelogEntry._with(rcp.changeMetadata(), changeMeta);
+
+        // Note: the changelogGroupName is derived dynamically,
+        // so here the compile-time checks against the DM via
+        // typed accessors cannot be used / would not make sense
+        // -> we use plain DocumentPointer and the _unwrap()'ed changelogEntry
+        String entityName = groupPointer.getPathParts().getLast().name();
+        String changelogGroupName = entityName + CHANGELOG_GROUP_SUFFIX + POINTER_WILDCARD;
+        DocumentPointer changelogGroupPointer =
+                groupPointer.parent().withConcatenated(DocumentPointer.of(changelogGroupName));
+        return result.withGroupRepetitionAppended(changelogGroupPointer, changelogEntry._unwrap());
+    }
+
+    private String createRepetitionDetails(GroupInstanceV2 groupInstance) {
+        return groupInstance.directFields().stream()
+                .filter(e -> !ID_FIELD_NAME.equals(e.getKey()))
+                .map(e -> "%s: %s".formatted(e.getKey(), e.getValue().value()))
+                .collect(Collectors.joining("\n"));
     }
 
 }
