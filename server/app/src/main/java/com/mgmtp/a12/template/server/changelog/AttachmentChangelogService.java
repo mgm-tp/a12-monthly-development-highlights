a300e632394fe102350471016deeca6fb6629f23
Gildardo Morga-Ramirez <gildardo.morga-ramirez@mgm-tp.com>
2026-03-06 14:44:08 +0100

    implement the changelog for the attachment Photo

--- server/app/src/main/java/com/mgmtp/a12/template/server/changelog/AttachmentChangelogService.java
+++ server/app/src/main/java/com/mgmtp/a12/template/server/changelog/AttachmentChangelogService.java
@@ -1,6 +1,11 @@
 package com.mgmtp.a12.template.server.changelog;
 
 import com.mgmtp.a12.kernel.md.document.apiV2.immutable.DocumentV2;
+import com.mgmtp.a12.template.server.typings.views.Person_DM;
+import com.mgmtp.a12.template.server.typings.views._attachmentchange_dm.AttachmentChange;
+import com.mgmtp.a12.template.server.typings.views._attachmentchange_dm._attachmentchange.NewAttachment;
+import com.mgmtp.a12.template.server.typings.views._attachmentchange_dm._attachmentchange.OldAttachment;
+import org.apache.commons.lang3.StringUtils;
 
 import java.time.Instant;
 
@@ -13,14 +18,41 @@ class AttachmentChangelogService extends CommonChangelogService {
     DocumentV2 addChangelogInfo(DocumentV2 document, DocumentV2 reference) {
         DocumentV2 result = document;
 
-        // use type accessor classes
+        Person_DM personDoc = Person_DM._viewOf(document);
+        Person_DM personRef = Person_DM._viewOf(reference);
 
-        // get attachment_id (be careful with NPE)
+        // trying to access via personDoc.person().personalData().photo().attachment_id() might lead to an NPE
+        // use _at instead
+        var attachmentIdPointer = Person_DM._pointer().person().personalData().photo().attachment_id();
+        String attachmentId = personDoc._at(attachmentIdPointer);
+        String attachmentIdRef = personRef._at(attachmentIdPointer);
 
-        // compare ids: new, updated, deleted
-
-        // add changelog entry to document
-
+        AttachmentChange changelogEntry = AttachmentChange._empty();
+        if (StringUtils.isNotBlank(attachmentId)) {
+            NewAttachment photo = NewAttachment._viewOf(personDoc.person().personalData().photo()._unwrap());
+            if (StringUtils.isBlank(attachmentIdRef)) {
+                // new attachment
+                changelogEntry = changelogEntry._with(AttachmentChange._pointer().newAttachment(), photo);
+            } else if (!attachmentId.equals(attachmentIdRef)) {
+                OldAttachment photoRef = OldAttachment._viewOf(personRef.person().personalData().photo()._unwrap());
+                changelogEntry = changelogEntry
+                        ._with(AttachmentChange._pointer().oldAttachment(), photoRef)
+                        ._with(AttachmentChange._pointer().newAttachment(), photo);
+            }
+            // no change
+        } else if (StringUtils.isNotBlank(attachmentIdRef)) {
+            // deleted attachment
+            OldAttachment photoRef = OldAttachment._viewOf(personRef.person().personalData().photo()._unwrap());
+            changelogEntry = changelogEntry._with(AttachmentChange._pointer().oldAttachment(), photoRef);
+        }
+        if (!changelogEntry.equals(AttachmentChange._empty())) {
+            result = addChangelogEntryToDocument(
+                    result,
+                    changelogEntry,
+                    AttachmentChange._pointer().changeMetadata(),
+                    Person_DM._pointer().person().personalData().photo()._unwrap()
+            );
+        }
         return result;
     }
 
